1766306c16858822fa96461a9c6452f8d1522cd4
Eric Bower <neurosnap@gmail.com>
2015-03-07 14:34:59 -0500

    major revision to Suspend methods

--- README.md
+++ README.md
@@ -9,6 +9,13 @@ Dependencies
 
 * There are none
 
+Supports
+--------
+
+* AMD
+* CommonJS
+* Globals
+
 Install
 -------
 
@@ -22,14 +29,15 @@ Tutorial
 Prevent a function to be actived when keys keep getting pressed
 
 ```
-var search_wait = new Suspend(function() {
-  console.log("If a key is pressed more than once every 500ms then I won't activate!");
+var search_wait = new Suspend();
+search_wait.callback(function() {
+  console.log("more than once every 200ms then I wouldn't have activated!");
 });
 
 document
   .getElementById("search-input")
-  .onkeypress = function(e) {
-    search_wait.every(500);
+  .onkeypress = function() {
+    search_wait.every(200).start();
   };
 ```
 
@@ -42,18 +50,40 @@ window.onresize = function(event) {
     .callback(function() {
       console.log("If window is resized more than once every 1000ms then I won't activate!");
     })
-    .every(1000);
+    .every(1000)
+    .start();
 };
+```
+
+Delay function until mouse is not longer hovering over eith element
+
+```
+var hover_wait = new Suspend({
+  element: [
+    document.getElementById("box-one"),
+    document.getElementById("box-two")
+  ],
+  event: "onmouseover",
+  callback: function(context, e) {
+    console.log(context, e);
+    console.log("If either box was hovered " + explain(1000));
+  }
+}).start();
 ```
 
 API
 ---
 
-Suspend(callback, data)
+Suspend(options)
 -----------------------
 
-* callback function -- function to be activated when the timer expires
-* data -- object that will be passed to the callback function
+* options -- object that will override and default settings
+
+.on(element, event)
+-------------------
+
+* element -- DOM element to apply event to, also accepts an array of elements
+* event -- string specifying the event: "onclick" or "onmouseover", also accepts an array of events
 
 .callback(cb)
 -------------
@@ -70,6 +100,23 @@ Suspend(callback, data)
 
 * timer -- integer in miliseconds that determines how long to wait for the event
 to be triggered before activating the callback function
+
+.start(timer)
+-------------
+
+* timer -- integer in miliseconds that determines how long to wait for the event
+to be triggered before activating the callback function
+
+Initiates Suspend
+
+.trigger(timer)
+---------------
+
+* timer -- integer in miliseconds that determines how long to wait for the event
+to be triggered before activating the callback function
+
+Alias for `start()`
+
 
 Credits
 -------
--- suspend.js
+++ suspend.js
@@ -1,39 +1,125 @@
-function Suspend(callback, data) {
-  this.timer;
-  this._data = data;
-  this._callback = callback;
+function Suspend(options) {
+  // private variables, only used internally
+  this._timer;
+
+  // combine Suspend instance with options
+  this.settings = this._merge({
+    "every": 500,
+    "event": [],
+    "element": []
+  }, options);
+
+  if (typeof this.settings.event === "string") {
+    this.settings.event = [this.settings.event];
+  }
+  if (typeof this.settings.element === "object" 
+      && typeof this.settings.element.length === "undefined") {
+        this.settings.element = [this.settings.element];
+  }
   return this;
 };
 
-Suspend.prototype.data = function(obj) {
-  if (typeof this._data !== "undefined") {
-    return this;
+Suspend.prototype._merge = function() {
+  var obj = {};
+  var key;
+  for (var i = 0, len = arguments.length; i < len; i++) {
+    for (key in arguments[i]) {
+      if (arguments[i].hasOwnProperty(key)) {
+        obj[key] = arguments[i][key];
+      }
+    }
   }
-  this._data = obj;
+  return obj;
+};
+
+Suspend.prototype.data = function(obj) {
+  this.settings.data = obj;
+  return this;
+};
+
+Suspend.prototype.on = function(element, ev) {
+  this.settings.element = element;
+  if (typeof ev === "string") {
+    this.settings.event.push(ev);
+  } else {
+    this.settings.event.concat(ev);
+  }
   return this;
 };
 
 Suspend.prototype.callback = function(cb) {
-  if (typeof this._callback !== "undefined") {
+  if (typeof this.settings.callback !== "undefined") {
     return this;
   }
-  this._callback = cb;
+  this.settings.callback = cb;
   return this;
 };
 
-Suspend.prototype.every = function(duration) {
+Suspend.prototype.element = function(element) {
+  this.settings.element = element;
+  return this;
+};
+
+Suspend.prototype.event = function(ev) {
+  this.settings.event = ev;
+  return this;
+}
+
+Suspend.prototype._events = function() {
   var that = this;
-  clearTimeout(that.timer);
-  that.timer = setTimeout(function() {
-    that._callback(that._data);
-  }, duration);
+  if (that.settings.event.length == 0) {
+    return that;
+  }
+  for (var j = 0, jlen = that.settings.element.length; j < jlen; j++) {
+    var el = that.settings.element[j];
+    for (var i = 0, len = that.settings.event.length; i < len; i++) {
+      var ev = that.settings.event[i];
+      el[ev] = function(e) {
+        that.data(e)._start();
+      };
+    }
+  }
+  return that;
+};
+
+Suspend.prototype.every = function(every) {
+  if (typeof every !== "number") {
+    throw "Must be integer in miliseconds!";
+  }
+  this.settings.every = every;
+  return this;
+};
+
+Suspend.prototype.start = function(every) {
+  if (typeof every === "number") {
+    this.settings.every = every;
+  }
+  if (this.settings.event.length == 0) {
+    this._start();
+    return this;
+  }
+  this._events();
+  return this;
+};
+
+Suspend.prototype.trigger = function(every) {
+  // alias for Suspend.start
+  return this.start(every);
+}
+
+Suspend.prototype._start = function() {
+  var that = this;
+  clearTimeout(that._timer);
+  that._timer = setTimeout(function() {
+    that.settings.callback(that, that.settings.data);
+  }, that.settings.every);
   return that;
 };
 
 (function(name, definition) {
-    if (typeof module != 'undefined') module.exports = definition();
-    else if (typeof define == 'function' && typeof define.amd == 'object') define(definition);
-    else this[name] = definition();
+  if (typeof module != 'undefined') module.exports = definition();
+  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition);
+  else this[name] = definition();
 }('mod', function() {
-    return Suspend;
+  return Suspend;
 }));
